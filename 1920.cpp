04e3c195d128c98400b9e2b29c3290ff888e10be
zcae1020 <zcae1020@naver.com>
2022-01-12 11:41:50 +0900

    1920v1

--- 1920.cpp
+++ 1920.cpp
@@ -1,31 +1,44 @@
-#include <iostream>
+#include <stdio.h>
 #include <algorithm>
-
 using namespace std;
 
-int main() {
-	ios::sync_with_stdio();
-	cin.tie();
-	cout.tie();
-
-	int N, M, arr[100005], input, flag;
-	cin >> N;
-	for (int i = 0;i < N;i++)
-		cin >> arr[i];
-
-	cin >> M;
-	for (int i = 0;i < M;i++) {
-		cin >> input;
-		flag = 1;
-		for (int j = 0;j < N;j++) {
-			if (arr[j] == input) {
-				printf("1\n");
-				flag = 0;
-				break;
-			}
+int arr[100001], m, n;
+int binarySearch(int *arr, int n, int m) {
+	int l, r, mid;
+	l = 0;
+	r = n;
+	while (l <= r)
+	{
+		mid = (l + r) / 2;
+		if (arr[mid] < m)
+		{
+			l = mid + 1;
 		}
-		if (flag)
-			printf("0\n");
+		else if (arr[mid] > m)
+		{
+			r = mid - 1;
+		}
+		else
+		{
+			return 1;
+		}
 	}
-
+	return 0;
+}
+int main()
+{
+	int n, m, arr[100001];
+	scanf("%d", &n);
+	for (int i = 0;i < n;i++)
+	{
+		scanf("%d", &arr[i]);
+	}
+	sort(arr, arr + n);
+	scanf("%d", &m);
+	for (int i = 0;i < m;i++)
+	{
+		int x;
+		scanf("%d", &x);
+		printf("%d\n", binarySearch(arr, n, x));
+	}
 }
