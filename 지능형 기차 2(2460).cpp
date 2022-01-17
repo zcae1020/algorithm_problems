7ad7bce673d33d90fe93f1aa90c6b79adacca64a
zcae1020 <zcae1020@naver.com>
2022-01-17 15:48:22 +0900

    2641v0

--- 지능형 기차 2(2460).cpp
+++ 지능형 기차 2(2460).cpp
@@ -1,16 +1,16 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-
-using namespace std;
-
-int main() {
-	int p = 0, mx = 0;
-	for (int i = 0;i < 10;i++) {
-		int up, down;
-		cin >> down >> up;
-		p -= down, p += up;
-		mx = max(mx, p);
-	}
-	cout << mx;
-}
+//#include <iostream>
+//#include <vector>
+//#include <algorithm>
+//
+//using namespace std;
+//
+//int main() {
+//	int p = 0, mx = 0;
+//	for (int i = 0;i < 10;i++) {
+//		int up, down;
+//		cin >> down >> up;
+//		p -= down, p += up;
+//		mx = max(mx, p);
+//	}
+//	cout << mx;
+//}
