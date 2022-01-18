59d9b4e41e86f9736784ec0a2be09967a743bc54
zcae1020 <zcae1020@naver.com>
2022-01-18 14:39:44 +0900

    5635v0

--- 다각형그리기(2641).cpp
+++ 다각형그리기(2641).cpp
@@ -1,72 +1,72 @@
-#include <iostream>
-#include <vector>
-
-using namespace std;
-
-int N, arrN, inst[50], insts[100][50], switc[50];
-vector<int> res;
-int to[5] = { 0,3,4,1,2 };
-
-void makeSwitc(int idx) {
-	int tmp[50];
-	for (int i = 0;i < N;i++)
-		tmp[i] = to[insts[idx][i]];
-	for (int i = 0;i < N;i++)
-		switc[i] = tmp[N - i - 1];
-}
-
-bool isEqual(int idx) {
-	makeSwitc(idx);
-	int start = inst[0];
-	int flag;
-	for (int i = 0;i < N;i++) {
-		flag = 1;
-		if (insts[idx][i] == start) {
-			for (int j = 1;j < N;j++) {
-				if (insts[idx][(i + j) % N] != inst[j]) {
-					flag = 0;
-					break;
-				}
-			}
-			if (flag)
-				return true;
-		}
-	}
-	for (int i = 0;i < N;i++) {
-		flag = 1;
-		if (switc[i] == start) {
-			for (int j = 1;j < N;j++) {
-				if (switc[(i + j) % N] != inst[j]) {
-					flag = 0;
-					break;
-				}
-			}
-			if (flag)
-				return true;
-		}
-	}
-	return false;
-}
-
-
-int main() {
-	cin >> N;
-	for (int i = 0;i < N;i++)
-		cin >> inst[i];
-
-	cin >> arrN;
-	for (int i = 0;i < arrN;i++) {
-		for (int j = 0;j < N;j++) {
-			cin >> insts[i][j];
-		}
-		if (isEqual(i))
-			res.emplace_back(i);
-	}
-
-	cout << res.size() << endl;
-	for (int i = 0;i < res.size();i++) {
-		for (int j = 0;j < N;j++)
-			cout << insts[res[i]][j] << ' ';
-		cout << endl;
-	}
-}
+//#include <iostream>
+//#include <vector>
+//
+//using namespace std;
+//
+//int N, arrN, inst[50], insts[100][50], switc[50];
+//vector<int> res;
+//int to[5] = { 0,3,4,1,2 };
+//
+//void makeSwitc(int idx) {
+//	int tmp[50];
+//	for (int i = 0;i < N;i++)
+//		tmp[i] = to[insts[idx][i]];
+//	for (int i = 0;i < N;i++)
+//		switc[i] = tmp[N - i - 1];
+//}
+//
+//bool isEqual(int idx) {
+//	makeSwitc(idx);
+//	int start = inst[0];
+//	int flag;
+//	for (int i = 0;i < N;i++) {
+//		flag = 1;
+//		if (insts[idx][i] == start) {
+//			for (int j = 1;j < N;j++) {
+//				if (insts[idx][(i + j) % N] != inst[j]) {
+//					flag = 0;
+//					break;
+//				}
+//			}
+//			if (flag)
+//				return true;
+//		}
+//	}
+//	for (int i = 0;i < N;i++) {
+//		flag = 1;
+//		if (switc[i] == start) {
+//			for (int j = 1;j < N;j++) {
+//				if (switc[(i + j) % N] != inst[j]) {
+//					flag = 0;
+//					break;
+//				}
+//			}
+//			if (flag)
+//				return true;
+//		}
+//	}
+//	return false;
+//}
+//
+//
+//int main() {
+//	cin >> N;
+//	for (int i = 0;i < N;i++)
+//		cin >> inst[i];
+//
+//	cin >> arrN;
+//	for (int i = 0;i < arrN;i++) {
+//		for (int j = 0;j < N;j++) {
+//			cin >> insts[i][j];
+//		}
+//		if (isEqual(i))
+//			res.emplace_back(i);
+//	}
+//
+//	cout << res.size() << endl;
+//	for (int i = 0;i < res.size();i++) {
+//		for (int j = 0;j < N;j++)
+//			cout << insts[res[i]][j] << ' ';
+//		cout << endl;
+//	}
+//}
---
+++ 생일(5635).cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+class person {
+
+public:
+	string name;
+	int year;
+	int month;
+	int day;
+
+	person(string name, int year, int month, int day) {
+		this->name = name;
+		this->year = year;
+		this->month = month;
+		this->day = day;
+	}
+};
+
+vector<person> v;
+
+bool compare(person p1, person p2) {
+	if (p1.year != p2.year)
+		return p1.year > p2.year;
+	else if (p1.month != p2.month)
+		return p1.month > p2.month;
+	return p1.day > p2.day;
+}
+
+int main() {
+	int N;
+	char str[20];
+	int d, m, y;
+	cin >> N;
+	for (int i = 0;i < N;i++) {
+		cin >> str >> d >> m >> y;
+		string tmp(str);
+		v.push_back(person(tmp, y, m, d));
+	}
+	sort(v.begin(), v.end(), compare);
+	cout << v[0].name.c_str() << endl << v[N - 1].name.c_str();
+}
