769b536ceab8ee61aa7cf65e09a44a2a4cbf7c1c
zcae1020 <zcae1020@naver.com>
2022-01-19 22:38:08 +0900

    20191v2 solved

--- 줄임말(20191).cpp
+++ 줄임말(20191).cpp
@@ -7,33 +7,29 @@
 using namespace std;
 
 char S[1000006], T[100005];
-int Salpa[26] = { 0 }, Talpa[26] = { 0 }, slen, tlen;
-double divide[26];
+vector<int> Ta[26];
+int slen, tlen;
 
 int main() {
 	cin >> S >> T;
 	slen = strlen(S), tlen = strlen(T);
 
-	for (int i = 0;i < slen;i++)
-		Salpa[S[i] - 'a']++;
 	for (int i = 0;i < tlen;i++)
-		Talpa[T[i] - 'a']++;
-	for (int i = 0;i < 26;i++) {
-		if (Salpa[i] && !Talpa[i]) {
+		Ta[T[i] - 'a'].push_back(i);
+	int sidx = 0, tidx = 0, res = 1, curAlpa;
+	while (sidx != slen) {
+		curAlpa = S[sidx] - 'a';
+		if (Ta[curAlpa].empty()) {
 			cout << -1;
 			return 0;
 		}
-	}
-	int sidx = 0, res = 1;
-	for (int i = 0;i <= tlen;i++) {
-		if (sidx == slen)
-			break;
-		if (i == tlen) {
+		int pos = lower_bound(Ta[curAlpa].begin(), Ta[curAlpa].end(), tidx) - Ta[curAlpa].begin();
+		if (pos >= Ta[curAlpa].size()) {
+			pos = 0;
 			res++;
-			i = 0;
 		}
-		if (S[sidx] == T[i])
-			sidx++;
+		tidx = Ta[curAlpa][pos] + 1;
+		sidx++;
 	}
 	cout << res;
 }
