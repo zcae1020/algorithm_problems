34badd638c6c50c2d350078f4b7605a8ece9fa98
zcae1020 <zcae1020@naver.com>
2022-01-19 21:25:25 +0900

    20191v1 score 42

--- 줄임말(20191).cpp
+++ 줄임말(20191).cpp
@@ -10,42 +10,30 @@ char S[1000006], T[100005];
 int Salpa[26] = { 0 }, Talpa[26] = { 0 }, slen, tlen;
 double divide[26];
 
-int compare() { //return: idx
-	int sidx = 0;
-	for (int i = 0;i < tlen;i++) {
-		if (S[sidx] == T[i])
-			sidx++;
-	}
-	return sidx;
-}
-
 int main() {
 	cin >> S >> T;
 	slen = strlen(S), tlen = strlen(T);
-	int c = compare();
-	if (c == slen) {
-		cout << 1;
-		return 0;
-	}
-	for (int i = c;i < slen;i++) {
+
+	for (int i = 0;i < slen;i++)
 		Salpa[S[i] - 'a']++;
-	}
-	for (int i = 0;i < tlen;i++) {
+	for (int i = 0;i < tlen;i++)
 		Talpa[T[i] - 'a']++;
-	}
 	for (int i = 0;i < 26;i++) {
-		if (Salpa[i] && Talpa[i])
-			divide[i] = (double)Salpa[i] / (double)Talpa[i];
-		else if (Salpa[i] && !Talpa[i]) {
+		if (Salpa[i] && !Talpa[i]) {
 			cout << -1;
 			return 0;
 		}
-		else
-			divide[i] = -1;
 	}
-	double mx = -1;
-	for (int i = 0;i < 26;i++) {
-		mx = max(mx, divide[i]);
+	int sidx = 0, res = 1;
+	for (int i = 0;i <= tlen;i++) {
+		if (sidx == slen)
+			break;
+		if (i == tlen) {
+			res++;
+			i = 0;
+		}
+		if (S[sidx] == T[i])
+			sidx++;
 	}
-	cout << ceil(mx) + 1;
+	cout << res;
 }
