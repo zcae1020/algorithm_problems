41350d81e45877d560022ca68e81f4f82f8c05e9
zcae1020 <zcae1020@naver.com>
2022-01-20 11:14:26 +0900

    2293v0

---
+++ 동전 1(2293).cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+
+int n, k, v[102], dp[10004] = { 0 };
+int main() {
+	cin >> n >> k;
+	for (int i = 0;i < n;i++)
+		cin >> v[i];
+
+	sort(v, v + n);
+
+	dp[0] = 1;
+	for (int i = 0;i < n;i++) {
+		for (int j = v[i];j <= k;j++) {
+			dp[j] = dp[j] + dp[j - v[i]];
+		}
+	}
+	cout << dp[k];
+}
--- 스티커(9465).cpp
+++ 스티커(9465).cpp
@@ -1,24 +1,24 @@
-#include <iostream>
-#include <algorithm>
-
-using namespace std;
-
-int T, n, map[2][100005], dp[2][100005];
-
-int main() {
-	cin >> T;
-	while (T--) {
-		cin >> n;
-		for (int i = 0;i < 2;i++) {
-			for (int j = 0;j < n;j++) {
-				cin >> map[i][j];
-			}
-		}
-		dp[0][0] = map[0][0], dp[1][0] = map[1][0];
-		for (int i = 1;i < n;i++) {
-			dp[0][i] = max(dp[0][i - 1], dp[1][i - 1] + map[0][i]);
-			dp[1][i] = max(dp[1][i - 1], dp[0][i - 1] + map[1][i]);
-		}
-		cout << max(dp[0][n - 1], dp[1][n - 1]) << endl;
-	}
-}
+//#include <iostream>
+//#include <algorithm>
+//
+//using namespace std;
+//
+//int T, n, map[2][100005], dp[2][100005];
+//
+//int main() {
+//	cin >> T;
+//	while (T--) {
+//		cin >> n;
+//		for (int i = 0;i < 2;i++) {
+//			for (int j = 0;j < n;j++) {
+//				cin >> map[i][j];
+//			}
+//		}
+//		dp[0][0] = map[0][0], dp[1][0] = map[1][0];
+//		for (int i = 1;i < n;i++) {
+//			dp[0][i] = max(dp[0][i - 1], dp[1][i - 1] + map[0][i]);
+//			dp[1][i] = max(dp[1][i - 1], dp[0][i - 1] + map[1][i]);
+//		}
+//		cout << max(dp[0][n - 1], dp[1][n - 1]) << endl;
+//	}
+//}
