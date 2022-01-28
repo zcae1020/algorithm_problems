74ede8a1620f95b705541b6cdf14d37e34e25f29
zcae1020 <zcae1020@naver.com>
2022-01-28 13:54:46 +0900

    16234v0

--- 괄호 추가하기(16637).cpp
+++ 괄호 추가하기(16637).cpp
@@ -1,41 +1,41 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-
-using namespace std;
-
-int N;
-char str[20];
-
-int cal(int idx, int num1, int num2) {
-	switch (str[idx]) {
-	case '+':
-		return num1 + num2;
-	case '-':
-		return num1 - num2;
-	case '*':
-		return num1 * num2;
-	}
-}
-
-int dfs(int idx, int cur) {
-	if (idx == N - 1)
-		return cur;
-	if (idx + 4 < N) {
-		int form = cal(idx + 1, cur, str[idx + 2] - '0');
-		int latt = cal(idx + 1, cur, cal(idx + 3, str[idx + 2] - '0', str[idx + 4] - '0'));
-		int fdfs = dfs(idx + 2, form), ldfs = dfs(idx + 4, latt);
-		int mx = fdfs > ldfs ? fdfs : ldfs;
-		return mx;
-	}
-	else if (idx + 2 < N) {
-		int d = cal(idx + 1, cur, str[idx + 2] - '0');
-		return dfs(idx + 2, d);
-	}
-}
-
-int main() {
-	cin >> N >> str;
-
-	cout << dfs(0, str[0] - '0');
-}
+//#include <iostream>
+//#include <vector>
+//#include <algorithm>
+//
+//using namespace std;
+//
+//int N;
+//char str[20];
+//
+//int cal(int idx, int num1, int num2) {
+//	switch (str[idx]) {
+//	case '+':
+//		return num1 + num2;
+//	case '-':
+//		return num1 - num2;
+//	case '*':
+//		return num1 * num2;
+//	}
+//}
+//
+//int dfs(int idx, int cur) {
+//	if (idx == N - 1)
+//		return cur;
+//	if (idx + 4 < N) {
+//		int form = cal(idx + 1, cur, str[idx + 2] - '0');
+//		int latt = cal(idx + 1, cur, cal(idx + 3, str[idx + 2] - '0', str[idx + 4] - '0'));
+//		int fdfs = dfs(idx + 2, form), ldfs = dfs(idx + 4, latt);
+//		int mx = fdfs > ldfs ? fdfs : ldfs;
+//		return mx;
+//	}
+//	else if (idx + 2 < N) {
+//		int d = cal(idx + 1, cur, str[idx + 2] - '0');
+//		return dfs(idx + 2, d);
+//	}
+//}
+//
+//int main() {
+//	cin >> N >> str;
+//
+//	cout << dfs(0, str[0] - '0');
+//}
