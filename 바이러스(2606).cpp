1573947c687a972acf9a887feb75769c4026be00
zcae1020 <zcae1020@naver.com>
2022-02-04 11:54:47 +0900

    2644v0

--- 바이러스(2606).cpp
+++ 바이러스(2606).cpp
@@ -1,35 +1,35 @@
-#include <iostream>
-#include <vector>
-#include <queue>
-
-using namespace std;
-
-int N, M, ans = 0, visited[101] = { 0 };
-vector<vector<int>> v;
-queue<int> q;
-
-int main() {
-	cin >> N >> M;
-	v.resize(N + 1);
-	for (int i = 0;i < M;i++) {
-		int a, b;
-		cin >> a >> b;
-		v[a].emplace_back(b);
-		v[b].emplace_back(a);
-	}
-	q.push(1);
-	visited[1] = 1;
-	while (!q.empty()) {
-		int cur = q.front();
-		q.pop();
-		for (int i = 0;i < v[cur].size();i++) {
-			int next = v[cur][i];
-			if (visited[next])
-				continue;
-			ans++;
-			visited[next] = 1;
-			q.push(next);
-		}
-	}
-	cout << ans;
-}
+//#include <iostream>
+//#include <vector>
+//#include <queue>
+//
+//using namespace std;
+//
+//int N, M, ans = 0, visited[101] = { 0 };
+//vector<vector<int>> v;
+//queue<int> q;
+//
+//int main() {
+//	cin >> N >> M;
+//	v.resize(N + 1);
+//	for (int i = 0;i < M;i++) {
+//		int a, b;
+//		cin >> a >> b;
+//		v[a].emplace_back(b);
+//		v[b].emplace_back(a);
+//	}
+//	q.push(1);
+//	visited[1] = 1;
+//	while (!q.empty()) {
+//		int cur = q.front();
+//		q.pop();
+//		for (int i = 0;i < v[cur].size();i++) {
+//			int next = v[cur][i];
+//			if (visited[next])
+//				continue;
+//			ans++;
+//			visited[next] = 1;
+//			q.push(next);
+//		}
+//	}
+//	cout << ans;
+//}
