f806336e566434cd2438a57c538a8574bc84bc01
zcae1020 <zcae1020@naver.com>
2022-02-08 07:49:53 +0900

    2294v0

---
+++ 동전 2(2294).cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+
+using namespace std;
+
+int n, k, coin[102], dp[10004];
+
+int main() {
+	cin >> n >> k;
+	for (int i = 0;i < n;i++)
+		cin >> coin[i];
+	for (int i = 1;i <= k;i++)
+		dp[i] = 10001;
+	dp[0] = 0;
+	for (int i = 1;i <= k;i++) {
+		for (int j = n - 1;j >= 0;j--) {
+			if (i >= coin[j])
+				dp[i] = min(dp[i], dp[i - coin[j]] + 1);
+		}
+	}
+	if (dp[k] == 10001)
+		cout << -1;
+	else
+		cout << dp[k];
+}
--- 맥주 먹으면서 걸어가기(9205).cpp
+++ 맥주 먹으면서 걸어가기(9205).cpp
@@ -1,50 +1,50 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <cstring>
-#define pii pair<int,int>
-
-using namespace std;
-
-int n, visited[102] = { 0 };
-pii h, f;
-vector<pii> c;
-
-bool dfs(pii cur) {
-	int dist = abs(cur.first - f.first) + abs(cur.second - f.second);
-	if (dist <= 1000)
-		return true;
-	for (int i = 0;i < n;i++) {
-		dist = abs(cur.first - c[i].first) + abs(cur.second - c[i].second);
-		if (visited[i] || dist > 1000)
-			continue;
-		visited[i] = 1;
-		bool tf = dfs(c[i]);
-		if (tf)
-			return true;
-	}
-	return false;
-}
-
-int main() {
-	int t, x, y;
-	cin >> t;
-	while (t--) {
-		cin >> n;
-		cin >> x >> y;
-		h.first = x, h.second = y;
-		for (int i = 0;i < n;i++) {
-			cin >> x >> y;
-			pii tmp = { x,y };
-			c.emplace_back(tmp);
-		}
-		cin >> x >> y;
-		f.first = x, f.second = y;
-		if (dfs(h))
-			cout << "happy" << endl;
-		else
-			cout << "sad" << endl;
-		c.clear();
-		memset(visited, 0, sizeof(visited));
-	}
-}
+//#include <iostream>
+//#include <vector>
+//#include <algorithm>
+//#include <cstring>
+//#define pii pair<int,int>
+//
+//using namespace std;
+//
+//int n, visited[102] = { 0 };
+//pii h, f;
+//vector<pii> c;
+//
+//bool dfs(pii cur) {
+//	int dist = abs(cur.first - f.first) + abs(cur.second - f.second);
+//	if (dist <= 1000)
+//		return true;
+//	for (int i = 0;i < n;i++) {
+//		dist = abs(cur.first - c[i].first) + abs(cur.second - c[i].second);
+//		if (visited[i] || dist > 1000)
+//			continue;
+//		visited[i] = 1;
+//		bool tf = dfs(c[i]);
+//		if (tf)
+//			return true;
+//	}
+//	return false;
+//}
+//
+//int main() {
+//	int t, x, y;
+//	cin >> t;
+//	while (t--) {
+//		cin >> n;
+//		cin >> x >> y;
+//		h.first = x, h.second = y;
+//		for (int i = 0;i < n;i++) {
+//			cin >> x >> y;
+//			pii tmp = { x,y };
+//			c.emplace_back(tmp);
+//		}
+//		cin >> x >> y;
+//		f.first = x, f.second = y;
+//		if (dfs(h))
+//			cout << "happy" << endl;
+//		else
+//			cout << "sad" << endl;
+//		c.clear();
+//		memset(visited, 0, sizeof(visited));
+//	}
+//}
