0e9dbf42fa999b79fed605b1447e4d1567a426b9
zcae1020 <zcae1020@naver.com>
2022-02-10 15:22:57 +0900

    1520v0

--- 타일 채우기(2133).cpp
+++ 타일 채우기(2133).cpp
@@ -1,20 +1,23 @@
-#include <iostream>
-
-using namespace std;
-
-int N;
-long long DP[31];
-
-int main() {
-	DP[2] = 3;
-	cin >> N;
-	if (N % 2)
-		cout << 0;
-	else {
-		for (int i = 4;i <= N;i += 2) {
-			DP[i] = DP[i - 2] * DP[2] + 2;
-		}
-		cout << DP[N];
-	}
-
-}
+//#include <iostream>
+//
+//using namespace std;
+//
+//int N;
+//long long DP[31];
+//
+//int main() {
+//	DP[0] = 1;
+//	DP[2] = 3;
+//	cin >> N;
+//	if (N % 2)
+//		cout << 0;
+//	else {
+//		for (int i = 4;i <= N;i += 2) {
+//			DP[i] = DP[i - 2] * 3;
+//			for (int j = i - 4;j >= 0;j -= 2) {
+//				DP[i] += 2 * DP[j];
+//			}
+//		}
+//		cout << DP[N];
+//	}
+//}
