9029b7399dc8045299180832a22cdeef6339f9ad
zcae1020 <zcae1020@naver.com>
2022-02-13 11:37:41 +0900

    2302v0

---
+++ 극장 좌석(2302).cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int N, M, fix[41] = { 0 }, dp[41];
+
+int main() {
+	cin >> N >> M;
+	for (int i = 0;i < M;i++) {
+		int x;
+		cin >> x;
+		fix[x] = 1;
+	}
+
+	dp[1] = 1;
+	if (fix[1] || fix[2])
+		dp[2] = 1;
+	else
+		dp[2] = 2;
+	for (int i = 3;i <= N;i++) {
+		if (fix[i - 1] || fix[i])
+			dp[i] = dp[i - 1];
+		else
+			dp[i] = dp[i - 1] + dp[i - 2];
+	}
+	cout << dp[N];
+}
--- 외판원 순회(2098).cpp
+++ 외판원 순회(2098).cpp
@@ -1,34 +1,34 @@
-#include <iostream>
-#include <algorithm>
-#define INF 2000000000
-
-using namespace std;
-
-int n, map[16][16], dp[16][1 << 16], VIS_ALL;
-
-int dfs(int last, int visited) {
-	if (visited == VIS_ALL)
-		return map[last][0] ? map[last][0] : INF;
-
-	if (dp[last][visited])
-		return dp[last][visited];
-
-	int mn = INF;
-	for (int i = 1;i < n;i++) {
-		if (visited & 1 << i || map[last][i] == 0)
-			continue;
-		mn = min(mn, dfs(i, visited | 1 << i) + map[last][i]);
-	}
-	dp[last][visited] = mn;
-	return mn;
-}
-
-int main() {
-	cin >> n;
-	VIS_ALL = (1 << n) - 1;
-	for (int i = 0;i < n;i++)
-		for (int j = 0;j < n;j++)
-			cin >> map[i][j];
-
-	cout << dfs(0, 1);
-}
+//#include <iostream>
+//#include <algorithm>
+//#define INF 2000000000
+//
+//using namespace std;
+//
+//int n, map[16][16], dp[16][1 << 16], VIS_ALL;
+//
+//int dfs(int last, int visited) {
+//	if (visited == VIS_ALL)
+//		return map[last][0] ? map[last][0] : INF;
+//
+//	if (dp[last][visited])
+//		return dp[last][visited];
+//
+//	int mn = INF;
+//	for (int i = 1;i < n;i++) {
+//		if (visited & 1 << i || map[last][i] == 0)
+//			continue;
+//		mn = min(mn, dfs(i, visited | 1 << i) + map[last][i]);
+//	}
+//	dp[last][visited] = mn;
+//	return mn;
+//}
+//
+//int main() {
+//	cin >> n;
+//	VIS_ALL = (1 << n) - 1;
+//	for (int i = 0;i < n;i++)
+//		for (int j = 0;j < n;j++)
+//			cin >> map[i][j];
+//
+//	cout << dfs(0, 1);
+//}
