7983329b839e05d415ddfc80322d2e48374a26ed
zcae1020 <zcae1020@naver.com>
2022-02-15 15:44:16 +0900

    1562v0

--- 공통 부분 문자열(5582).cpp
+++ 공통 부분 문자열(5582).cpp
@@ -1,21 +1,21 @@
-#include <iostream>
-#include <string>
-#include <algorithm>
-
-using namespace std;
-
-string a, b;
-int dp[4003][4003] = { 0 }, mx = 0;
-
-int main() {
-	cin >> a >> b;
-	for (int i = 1;i <= a.size();i++) {
-		for (int j = 1;j <= b.size();j++) {
-			if (a[i - 1] == b[j - 1]) {
-				dp[i][j] = dp[i - 1][j - 1] + 1;
-				mx = max(mx, dp[i][j]);
-			}
-		}
-	}
-	cout << mx;
-}
+//#include <iostream>
+//#include <string>
+//#include <algorithm>
+//
+//using namespace std;
+//
+//string a, b;
+//int dp[4003][4003] = { 0 }, mx = 0;
+//
+//int main() {
+//	cin >> a >> b;
+//	for (int i = 1;i <= a.size();i++) {
+//		for (int j = 1;j <= b.size();j++) {
+//			if (a[i - 1] == b[j - 1]) {
+//				dp[i][j] = dp[i - 1][j - 1] + 1;
+//				mx = max(mx, dp[i][j]);
+//			}
+//		}
+//	}
+//	cout << mx;
+//}
