46cc65753f93a652a6da6e0d0d3ce6e80056f1b7
zcae1020 <zcae1020@naver.com>
2022-03-01 22:51:07 +0900

    220301v1

---
+++ 가르침(1062).cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+bool alpa[26];
+int N, K;
+string input;
+char antic[5] = { 'a','n','t','i','c' };
+vector<string> v;
+
+int canRead() {
+	int ret = 0, flag;
+	for (int i = 0;i < v.size();i++) {
+		flag = 1;
+		for (int j = 0;j < v[i].size();j++) {
+			if (!alpa[v[i][j] - 'a']) {
+				flag = 0;
+				break;
+			}
+		}
+		if (flag)
+			ret++;
+	}
+	return ret;
+}
+
+int ans(int idx, int cnt) {
+	int ret = 0;
+	if (cnt == K)
+		return canRead();
+	for (int i = idx;i < 26;i++) {
+		if (!alpa[i]) {
+			alpa[i] = true;
+			ret = max(ret, ans(i + 1, cnt + 1));
+			alpa[i] = false;
+		}
+	}
+	return ret;
+}
+
+int main() {
+	cin >> N >> K;
+	K = K - 5;
+	if (K < 0) {
+		cout << 0;
+		return 0;
+	}
+
+	for (int i = 0;i < 5;i++)
+		alpa[antic[i] - 'a'] = true;
+	
+	v.resize(N);
+	for (int i = 0;i < N;i++)
+		cin >> v[i];
+
+	cout << ans(0, 0);
+}
--- 택배(8980).cpp
+++ 택배(8980).cpp
@@ -1,57 +1,57 @@
-#include <iostream>
-#include <algorithm>
-#include <vector>
-
-using namespace std;
-
-typedef struct tae {
-	int from;
-	int to;
-	int box;
-}Ta;
-
-bool comp(Ta t1, Ta t2) {
-	if (t1.to == t2.to)
-		return t1.from < t2.from;
-	return t1.to < t2.to;
-}
-
-int N, C, M;
-int map[2001];
-vector<Ta> v;
-
-int getMax(Ta t) {
-	int ret = 0;
-	for (int i = t.from;i < t.to;i++)
-		ret = max(ret, map[i]);
-	return ret;
-}
-
-void putValue(Ta t, int value) {
-	for (int i = t.from;i < t.to;i++)
-		map[i] += value;
-}
-
-int main() {
-	cin >> N >> C >> M;
-	for (int i = 0;i < M;i++) {
-		Ta t;
-		cin >> t.from >> t.to >> t.box;
-		v.push_back(t);
-	}
-
-	sort(v.begin(), v.end(), comp);
-
-	int ans = 0, mx, value;
-	for (int i = 0;i < M;i++) {
-		mx = getMax(v[i]);
-		if (C - mx > v[i].box)
-			value = v[i].box;
-		else
-			value = C - mx;
-		ans += value;
-		putValue(v[i], value);
-	}
-
-	cout << ans;
-}
+//#include <iostream>
+//#include <algorithm>
+//#include <vector>
+//
+//using namespace std;
+//
+//typedef struct tae {
+//	int from;
+//	int to;
+//	int box;
+//}Ta;
+//
+//bool comp(Ta t1, Ta t2) {
+//	if (t1.to == t2.to)
+//		return t1.from < t2.from;
+//	return t1.to < t2.to;
+//}
+//
+//int N, C, M;
+//int map[2001];
+//vector<Ta> v;
+//
+//int getMax(Ta t) {
+//	int ret = 0;
+//	for (int i = t.from;i < t.to;i++)
+//		ret = max(ret, map[i]);
+//	return ret;
+//}
+//
+//void putValue(Ta t, int value) {
+//	for (int i = t.from;i < t.to;i++)
+//		map[i] += value;
+//}
+//
+//int main() {
+//	cin >> N >> C >> M;
+//	for (int i = 0;i < M;i++) {
+//		Ta t;
+//		cin >> t.from >> t.to >> t.box;
+//		v.push_back(t);
+//	}
+//
+//	sort(v.begin(), v.end(), comp);
+//
+//	int ans = 0, mx, value;
+//	for (int i = 0;i < M;i++) {
+//		mx = getMax(v[i]);
+//		if (C - mx > v[i].box)
+//			value = v[i].box;
+//		else
+//			value = C - mx;
+//		ans += value;
+//		putValue(v[i], value);
+//	}
+//
+//	cout << ans;
+//}
