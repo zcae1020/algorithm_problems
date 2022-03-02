d7337eabf5e8e71f28ac4859218a03f15c26f662
zcae1020 <zcae1020@naver.com>
2022-03-02 12:01:44 +0900

    220302v0

--- 가르침(1062).cpp
+++ 가르침(1062).cpp
@@ -1,60 +1,77 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <string>
-
-using namespace std;
-
-bool alpa[26];
-int N, K;
-string input;
-char antic[5] = { 'a','n','t','i','c' };
-vector<string> v;
-
-int canRead() {
-	int ret = 0, flag;
-	for (int i = 0;i < v.size();i++) {
-		flag = 1;
-		for (int j = 0;j < v[i].size();j++) {
-			if (!alpa[v[i][j] - 'a']) {
-				flag = 0;
-				break;
-			}
-		}
-		if (flag)
-			ret++;
-	}
-	return ret;
-}
-
-int ans(int idx, int cnt) {
-	int ret = 0;
-	if (cnt == K)
-		return canRead();
-	for (int i = idx;i < 26;i++) {
-		if (!alpa[i]) {
-			alpa[i] = true;
-			ret = max(ret, ans(i + 1, cnt + 1));
-			alpa[i] = false;
-		}
-	}
-	return ret;
-}
-
-int main() {
-	cin >> N >> K;
-	K = K - 5;
-	if (K < 0) {
-		cout << 0;
-		return 0;
-	}
-
-	for (int i = 0;i < 5;i++)
-		alpa[antic[i] - 'a'] = true;
-	
-	v.resize(N);
-	for (int i = 0;i < N;i++)
-		cin >> v[i];
-
-	cout << ans(0, 0);
-}
+//#include <iostream>
+//#include <vector>
+//#include <algorithm>
+//#include <string>
+//
+//using namespace std;
+//
+//bool alpa[26];
+//int N, K;
+//string input;
+//char antic[5] = { 'a','n','t','i','c' };
+//vector<vector<char>> v;
+//
+//int canRead() {
+//	int ret = 0, flag;
+//	for (int i = 0;i < v.size();i++) {
+//		flag = 1;
+//		for (int j = 0;j < v[i].size();j++) {
+//			if (!alpa[v[i][j] - 'a']) {
+//				flag = 0;
+//				break;
+//			}
+//		}
+//		if (flag)
+//			ret++;
+//	}
+//	return ret;
+//}
+//
+//int ans(int rest, int idx) {
+//	int ret = 0;
+//	if (rest == 0)
+//		return canRead();
+//
+//	for (int i = idx;i < 26;i++) {
+//		if (!alpa[i]) {
+//			alpa[i] = true;
+//			ret = max(ret, ans(rest - 1, i + 1));
+//			alpa[i] = false;
+//		}
+//	}
+//	return ret;
+//}
+//
+//int main() {
+//	cin >> N >> K;
+//	K -= 5;
+//	if (K < 0) {
+//		cout << 0;
+//		return 0;
+//	}
+//	for (int i = 0;i < 5;i++)
+//		alpa[antic[i] - 'a'] = true;
+//
+//	vector<char> tmp;
+//	while (N--) {
+//		cin >> input;
+//		tmp.clear();
+//
+//		for (int i = 4;i < input.size() - 4;i++) {
+//			if (alpa[input[i] - 'a'])
+//				continue;
+//			tmp.push_back(input[i]);
+//			alpa[input[i] - 'a'] = true;
+//		}
+//
+//		for (int i = 0;i < tmp.size();i++)
+//			alpa[tmp[i] - 'a'] = false;
+//
+//		if (tmp.size() > K)
+//			continue;
+//
+//		v.push_back(tmp);
+//	}
+//
+//	cout << ans(K, 0);
+//}
---
+++ 마피아(1079).cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+bool die[16];
+int N, guilt[16], R[16][16], ej;
+
+void Input() {
+	cin >> N;
+
+	for (int i = 0; i < N; i++)
+		cin >> guilt[i];
+
+	for (int i = 0; i < N; i++)
+		for (int j = 0; j < N; j++)
+			cin >> R[i][j];
+
+	cin >> ej;
+}
+
+void changeGuilt(int idx) {
+	for (int i = 0; i < N; i++)
+		guilt[i] += R[idx][i];
+}
+
+void initGuilt(int idx) {
+	for (int i = 0; i < N; i++)
+		guilt[i] -= R[idx][i];
+}
+
+int afternoon() {
+	int mx = 0;
+	for (int i = 0; i < N; i++) {
+		if (die[i])continue;
+		mx = max(mx, guilt[i]);
+	}
+	for (int i = 0; i < N; i++) {
+		if (die[i])continue;
+		if (mx == guilt[i])
+			return i;
+	}
+}
+
+int dfs(int day) {
+	int ret = day, a;
+	for (int i = 0; i < N; i++) {
+		if (i == ej || die[i])
+			continue;
+		die[i] = true;
+		changeGuilt(i);
+
+		a = afternoon();
+		if (a != ej) {
+			die[a] = true;
+			ret = max(ret, dfs(day + 1));
+			die[a] = false;
+		}
+
+		initGuilt(i);
+		die[i] = false;
+	}
+	return ret;
+}
+
+int main() {
+	Input();
+
+	if (N % 2) {
+		if (afternoon()) {
+			cout << 0;
+			return 0;
+		}
+	}
+
+	cout << dfs(1);
+}
