96195de735a5133faacaa1f42473845a1b351a89
zcae1020 <zcae1020@naver.com>
2022-03-04 14:05:25 +0900

    220304v0

---
+++ 달이 차오른다, 가자.(1194).cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+#include <algorithm>
+#include <queue>
+#include <cstring>
+#include <tuple>
+#define pii pair<int,int>
+
+using namespace std;
+
+int N, M;
+bool visited[50][50][64];
+char map[51][51];
+queue<pair<pii, pii>> q;
+pii z;
+
+int dx[4] = { 0,0,1,-1 }, dy[4] = { 1,-1,0,0 };
+
+void Input() {
+	cin >> N >> M;
+	for (int i = 0; i < N; i++) {
+		cin >> map[i];
+		for (int j = 0; j < M; j++) {
+			if (map[i][j] == '0') {
+				z = make_pair(j, i);
+				map[i][j] = '.';
+			}
+		}
+	}
+}
+
+bool haveKey(int k, char d) {
+	int dtoi = 1 << (d - 'A');
+	return k & dtoi;
+}
+
+int bfs() {
+	q.push(make_pair(z, make_pair(0, 0)));
+	visited[z.second][z.first][0] = true;
+
+	while (!q.empty()) {
+		pii cur = q.front().first;
+		int cnt = q.front().second.first;
+		int key = q.front().second.second;
+		q.pop();
+
+		for (int i = 0; i < 4; i++) {
+			int nx = cur.first + dx[i], ny = cur.second + dy[i];
+			
+			if (nx < 0 || ny < 0 || nx >= M || ny >= N)
+				continue;
+
+			if (!visited[ny][nx][key]) {
+				
+				if (map[ny][nx] == '1')
+					return cnt + 1;
+				
+				if (map[ny][nx] == '.') {
+					visited[ny][nx][key] = true;
+					q.push(make_pair(make_pair(nx, ny), make_pair(cnt + 1, key)));
+				}
+
+				if (map[ny][nx] >= 'A' && map[ny][nx] <= 'F') {
+					if (haveKey(key, map[ny][nx])) {
+						visited[ny][nx][key] = true;
+						q.push(make_pair(make_pair(nx, ny), make_pair(cnt + 1, key)));
+					}
+				}
+
+				if (map[ny][nx] >= 'a' && map[ny][nx] <= 'f') {
+					int tmpK = key | (1 << (map[ny][nx] - 'a'));
+					visited[ny][nx][tmpK] = true;
+					q.push(make_pair(make_pair(nx, ny), make_pair(cnt + 1, tmpK)));
+				}
+			}
+		}
+	}
+	return -1;
+}
+
+int main() {
+	Input();
+	cout << bfs();
+}
--- 중량제한(1939).cpp
+++ 중량제한(1939).cpp
@@ -1,65 +1,65 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <queue>
-#include <cstring>
-#define MAX 10001
-#define pii pair<int,int>
-
-using namespace std;
-
-int N, M, A, B, MC, visited[MAX];
-vector<vector<pii>> v(MAX);
-
-void Input() {
-	cin >> N >> M;
-	for (int i = 0; i < M; i++) {
-		int a, b, c;
-		cin >> a >> b >> c;
-
-		MC = max(MC, c);
-		v[a].push_back(make_pair(b, c));
-		v[b].push_back(make_pair(a, c));
-	}
-	cin >> A >> B;
-}
-
-bool BFS(int cost) {
-	queue<int> q;
-	q.push(A);
-	visited[A] = true;
-
-	while (!q.empty()) {
-		int cur = q.front();
-		q.pop();
-
-		if (cur == B)
-			return true;
-
-		for (pii p : v[cur]) {
-			int to = p.first, w = p.second;
-			if (visited[to] || w < cost)
-				continue;
-
-			q.push(to);
-			visited[to] = true;
-		}
-	}
-	return false;
-}
-
-int BS() {
-	int l = 1, r = MC + 1, m;
-	while (l + 1 < r) {
-		memset(visited, 0, sizeof(visited));
-		m = (l + r) / 2;
-		if (BFS(m)) l = m;
-		else r = m;
-	}
-	return l;
-}
-
-int main() {
-	Input();
-	cout << BS();
-}
+//#include <iostream>
+//#include <vector>
+//#include <algorithm>
+//#include <queue>
+//#include <cstring>
+//#define MAX 10001
+//#define pii pair<int,int>
+//
+//using namespace std;
+//
+//int N, M, A, B, MC, visited[MAX];
+//vector<vector<pii>> v(MAX);
+//
+//void Input() {
+//	cin >> N >> M;
+//	for (int i = 0; i < M; i++) {
+//		int a, b, c;
+//		cin >> a >> b >> c;
+//
+//		MC = max(MC, c);
+//		v[a].push_back(make_pair(b, c));
+//		v[b].push_back(make_pair(a, c));
+//	}
+//	cin >> A >> B;
+//}
+//
+//bool BFS(int cost) {
+//	queue<int> q;
+//	q.push(A);
+//	visited[A] = true;
+//
+//	while (!q.empty()) {
+//		int cur = q.front();
+//		q.pop();
+//
+//		if (cur == B)
+//			return true;
+//
+//		for (pii p : v[cur]) {
+//			int to = p.first, w = p.second;
+//			if (visited[to] || w < cost)
+//				continue;
+//
+//			q.push(to);
+//			visited[to] = true;
+//		}
+//	}
+//	return false;
+//}
+//
+//int BS() {
+//	int l = 1, r = MC + 1, m;
+//	while (l + 1 < r) {
+//		memset(visited, 0, sizeof(visited));
+//		m = (l + r) / 2;
+//		if (BFS(m)) l = m;
+//		else r = m;
+//	}
+//	return l;
+//}
+//
+//int main() {
+//	Input();
+//	cout << BS();
+//}
