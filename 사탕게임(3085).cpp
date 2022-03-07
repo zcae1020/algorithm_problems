1319952223a1575ed8850cf5d4a8e4382b803b51
zcae1020 <zcae1020@naver.com>
2022-03-07 14:06:32 +0900

    220307v0

---
+++ 사탕게임(3085).cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int N, mx = 0;
+char map[50][51];
+int dx[2] = { 0,1 }, dy[2] = { 1,0 };
+int lx[4] = { 0,0,1,-1 }, ly[4] = { 1,-1,0,0 };
+
+int maxLength(int x, int y) {
+	int ret = 0;
+	int sum = 1, nx, ny;
+	for (int i = 0; i < 2; i++) {
+		nx = x, ny = y;
+		while (true) {
+			nx += lx[i], ny += ly[i];
+			if (map[ny][nx] != map[y][x] || nx < 0 || ny < 0 || nx >= N || ny >= N)
+				break;
+			sum++;
+		}
+	}
+	ret = max(ret, sum);
+
+	sum = 1;
+	for (int i = 2; i < 4; i++) {
+		nx = x, ny = y;
+		while (true) {
+			nx += lx[i], ny += ly[i];
+			if (map[ny][nx] != map[y][x] || nx < 0 || ny < 0 || nx >= N || ny >= N)
+				break;
+			sum++;
+		}
+	}
+	ret = max(ret, sum);
+
+	return ret;
+}
+
+int main() {
+	cin >> N;
+	for (int i = 0; i < N; i++)
+		cin >> map[i];
+
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			mx = max(mx, maxLength(j, i));
+		}
+	}
+	if (mx == N) {
+		cout << mx;
+		return 0;
+	}
+
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			for (int k = 0; k < 2; k++) {
+				int nx = j + dx[k], ny = i + dy[k];
+				if (map[i][j] == map[ny][nx] || nx >= N || ny >= N) continue;
+				
+				char tmp;
+				tmp = map[i][j], map[i][j] = map[ny][nx], map[ny][nx] = tmp;
+
+				mx = max(mx, max(maxLength(j, i), maxLength(nx, ny)));
+
+				tmp = map[i][j], map[i][j] = map[ny][nx], map[ny][nx] = tmp;
+			}
+		}
+	}
+	cout << mx;
+}
--- 체스(1986).cpp
+++ 체스(1986).cpp
@@ -1,73 +1,73 @@
-#include <iostream>
-
-using namespace std;
-
-int N, M, map[1000][1000];
-bool visited[1000][1000];
-
-int qx[8] = { 0,0,1,-1,1,1,-1,-1 }, qy[8] = { 1,-1,0,0,1,-1,1,-1 };
-int kx[8] = { 1,1,2,2,-1,-1,-2,-2 }, ky[8] = { -2,2,-1,1,-2,2,-1,1 };
-
-void input() {
-	cin >> N >> M;
-	int n, x, y;
-	for (int i = 1; i <= 3; i++) {
-		cin >> n;
-		while (n--) {
-			cin >> y >> x;
-			map[y - 1][x - 1] = i;
-		}
-	}
-}
-
-
-
-void draw() {
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < M; j++) {
-			switch (map[i][j]) {
-			case 1:
-				visited[i][j] = true;
-				for (int k = 0; k < 8; k++) {
-					int cx = j, cy = i;
-					while (true) {
-						cx += qx[k], cy += qy[k];
-						if (map[cy][cx] || cx < 0 || cy < 0 || cx >= M || cy >= N)
-							break;
-						visited[cy][cx] = true;
-					}
-				}
-				break;
-			case 2:
-				visited[i][j] = true;
-				for (int k = 0; k < 8; k++) {
-					int cx = j + kx[k], cy = i + ky[k];
-					if (map[cy][cx] || cx < 0 || cy < 0 || cx >= M || cy >= N)
-						continue;
-					visited[cy][cx] = true;
-				}
-				break;
-			case 3:
-				visited[i][j] = true;
-				break;
-			}
-		}
-	}
-}
-
-int getSafety() {
-	int ret = 0;
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < M; j++) {
-			if (visited[i][j] == false)
-				ret++;
-		}
-	}
-	return ret;
-}
-
-int main() {
-	input();
-	draw();
-	cout << getSafety();
-}
+//#include <iostream>
+//
+//using namespace std;
+//
+//int N, M, map[1000][1000];
+//bool visited[1000][1000];
+//
+//int qx[8] = { 0,0,1,-1,1,1,-1,-1 }, qy[8] = { 1,-1,0,0,1,-1,1,-1 };
+//int kx[8] = { 1,1,2,2,-1,-1,-2,-2 }, ky[8] = { -2,2,-1,1,-2,2,-1,1 };
+//
+//void input() {
+//	cin >> N >> M;
+//	int n, x, y;
+//	for (int i = 1; i <= 3; i++) {
+//		cin >> n;
+//		while (n--) {
+//			cin >> y >> x;
+//			map[y - 1][x - 1] = i;
+//		}
+//	}
+//}
+//
+//
+//
+//void draw() {
+//	for (int i = 0; i < N; i++) {
+//		for (int j = 0; j < M; j++) {
+//			switch (map[i][j]) {
+//			case 1:
+//				visited[i][j] = true;
+//				for (int k = 0; k < 8; k++) {
+//					int cx = j, cy = i;
+//					while (true) {
+//						cx += qx[k], cy += qy[k];
+//						if (map[cy][cx] || cx < 0 || cy < 0 || cx >= M || cy >= N)
+//							break;
+//						visited[cy][cx] = true;
+//					}
+//				}
+//				break;
+//			case 2:
+//				visited[i][j] = true;
+//				for (int k = 0; k < 8; k++) {
+//					int cx = j + kx[k], cy = i + ky[k];
+//					if (map[cy][cx] || cx < 0 || cy < 0 || cx >= M || cy >= N)
+//						continue;
+//					visited[cy][cx] = true;
+//				}
+//				break;
+//			case 3:
+//				visited[i][j] = true;
+//				break;
+//			}
+//		}
+//	}
+//}
+//
+//int getSafety() {
+//	int ret = 0;
+//	for (int i = 0; i < N; i++) {
+//		for (int j = 0; j < M; j++) {
+//			if (visited[i][j] == false)
+//				ret++;
+//		}
+//	}
+//	return ret;
+//}
+//
+//int main() {
+//	input();
+//	draw();
+//	cout << getSafety();
+//}
