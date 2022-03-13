ca6ec76ad13de14dc564fd74ed4701d8963b0d61
zcae1020 <zcae1020@naver.com>
2022-03-13 20:57:31 +0900

    220313v2

--- 준표의 조약돌(15831).cpp
+++ 준표의 조약돌(15831).cpp
@@ -1,30 +1,30 @@
-#include <iostream>
-#include <algorithm>
-
-using namespace std;
-
-int N, B, W, b, w, s, ans;
-char a[300000];
-
-int main() {
-	cin >> N >> B >> W;
-
-	for (int i = 0; i < N; i++) {
-		cin >> a[i];
-
-		if (a[i] == 'W')
-			w++;
-		if (a[i] == 'B') {
-			b++;
-			while (b > B) {
-				if (a[s] == 'W') w--;
-				if (a[s] == 'B') b--;
-				s++;
-			}
-		}
-
-		if (w >= W && b <= B) ans = max(ans, i - s + 1);
-	}
-
-	cout << ans;
-}
+//#include <iostream>
+//#include <algorithm>
+//
+//using namespace std;
+//
+//int N, B, W, b, w, s, ans;
+//char a[300000];
+//
+//int main() {
+//	cin >> N >> B >> W;
+//
+//	for (int i = 0; i < N; i++) {
+//		cin >> a[i];
+//
+//		if (a[i] == 'W')
+//			w++;
+//		if (a[i] == 'B') {
+//			b++;
+//			while (b > B) {
+//				if (a[s] == 'W') w--;
+//				if (a[s] == 'B') b--;
+//				s++;
+//			}
+//		}
+//
+//		if (w >= W && b <= B) ans = max(ans, i - s + 1);
+//	}
+//
+//	cout << ans;
+//}
