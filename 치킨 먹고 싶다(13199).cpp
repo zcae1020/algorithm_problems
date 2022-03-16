cb5f748f3506f99e764b02915c27327e06894297
zcae1020 <zcae1020@naver.com>
2022-03-16 13:44:12 +0900

    220316v0

--- 치킨 먹고 싶다(13199).cpp
+++ 치킨 먹고 싶다(13199).cpp
@@ -1,18 +1,19 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include <iostream>
-
-using namespace std;
-
-int T, P, M, F, C;
-int sang, du, coupon;
-
-int main() {
-	scanf("%d", &T);
-	while (T--) {
-		scanf("%d %d %d %d", &P, &M, &F, &C);
-		coupon = (M / P) * C;
-		du = coupon / F;
-		if (coupon >= F) sang = (coupon - F) / (F - C) + 1;
-		printf("%d\n", sang - du);
-	}
-}
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <iostream>
+//
+//using namespace std;
+//
+//int T, P, M, F, C;
+//int sang, du, coupon;
+//
+//int main() {
+//	scanf("%d", &T);
+//	while (T--) {
+//		scanf("%d %d %d %d", &P, &M, &F, &C);
+//		coupon = (M / P) * C;
+//		du = M / P + coupon / F;
+//		sang = M / P;
+//		if (coupon >= F) sang += (coupon - F) / (F - C) + 1;
+//		printf("%d\n", sang - du);
+//	}
+//}
