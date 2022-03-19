56fe578fba983a8a9554614d5e0a3e7596e3c495
zcae1020 <zcae1020@naver.com>
2022-03-19 21:04:48 +0900

    220319v1

---
+++ 새로운 게임(17780).cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#define pii pair<int,int>
+
+using namespace std;
+
+int N, K, map[12][12];
+pair<pii, int>u[10];
+vector<int> umap[12][12];
+
+int dx[4] = { 0,0,-1,1 }, dy[4] = { 1,-1,0,0 };
+
+void Input() {
+	cin >> N >> K;
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			cin >> map[i][j];
+		}
+	}
+
+	for (int i = 0; i < K; i++) {
+		int x, y, d;
+		cin >> x >> y >> d;
+		umap[x - 1][y - 1].push_back(i);
+		u[i] = { {x - 1,y - 1},d - 1 };
+	}
+}
+
+int changeD(int d) {
+	if (d == 0)d = 1;
+	else if (d == 1)d = 0;
+	else if (d == 2)d = 3;
+	else if (d == 3)d = 2;
+	return d;
+}
+
+pii move(int uIdx) {
+	pair<pii, int> *cur = &u[uIdx];
+	pii cp = cur->first;
+	pii np = { cur->first.first + dx[cur->second], cur->first.second + dy[cur->second] };
+
+	if (np.first < 0 || np.second < 0 || np.first >= N || np.second >= N || map[np.first][np.second] == 2) {
+		u[uIdx].second = changeD(u[uIdx].second);
+		np = { cp.first + dx[cur->second],cp.second + dy[cur->second] };
+
+		if (np.first >= 0 && np.second >= 0 && np.first < N && np.second < N && map[np.first][np.second] != 2) return move(uIdx);
+		return cp;
+	}
+	else if (map[np.first][np.second] == 1)
+		reverse(umap[cp.first][cp.second].begin(), umap[cp.first][cp.second].end());
+
+	for (auto a : umap[cp.first][cp.second]) {
+		u[a] = { {np.first,np.second},u[a].second };
+		umap[np.first][np.second].push_back(a);
+	}
+	umap[cp.first][cp.second].clear();
+	return np;
+}
+
+void Solution() {
+	int turn = 0;
+	while (turn <= 1000) {
+		turn++;
+		for (int i = 0; i < K; i++) {
+			pair<pii, int> cur = u[i];
+			if (umap[cur.first.first][cur.first.second][0] == i) {
+				pii p = move(i);
+				if (umap[p.first][p.second].size() >= 4) {
+					cout << turn;
+					return;
+				}
+			}
+		}
+	}
+	cout << -1;
+}
+
+int main() {
+	Input();
+	Solution();
+}
--- 주사위 윷놀이(17825).cpp
+++ 주사위 윷놀이(17825).cpp
@@ -1,102 +1,102 @@
-#include <iostream>
-
-using namespace std;
-
-int input[10], map[33], answer; // 0:start, 21:finsh
-
-int isNext(int idx, int move) {
-	int nidx, nmove;
-
-	if (move == 0) return idx;
-
-	if (idx == 5)
-		return isNext(22, move - 1);
-	else if (idx == 10)
-		return isNext(25, move - 1);
-	else if (idx == 15)
-		return isNext(27, move - 1);
-	else if (idx >= 22) {
-		if (idx < 25) {
-			if (idx + move > 24) {
-				nmove = move - (25 - idx);
-				nidx = 30;
-				return isNext(nidx, nmove);
-			}
-			else {
-				return idx + move;
-			}
-		}
-		else if (idx < 27) {
-			if (idx + move > 26) {
-				nmove = move - (27 - idx);
-				nidx = 30;
-				return isNext(nidx, nmove);
-			}
-			else {
-				return idx + move;
-			}
-		}
-		else if (idx < 30) {
-			if (idx + move > 29) {
-				nmove = move - (30 - idx);
-				nidx = 30;
-				return isNext(nidx, nmove);
-			}
-			else {
-				return idx + move;
-			}
-		}
-		else if (idx < 33) {
-			if (idx + move > 32) {
-				nmove = move - (33 - idx);
-				nidx = 20;
-				return isNext(nidx, nmove);
-			}
-			else {
-				return idx + move;
-			}
-		}
-	}
-	else {
-		nidx = idx + move;
-		if (nidx > 20) return 21;
-		return nidx;
-	}
-}
-
-int getAnswer(int n) {
-	if (n == 21 || n == 0) return 0;
-
-	const int score[11] = {
-		13,16,19,22,24,28,27,26,25,30,35
-	};
-
-	if (n < 22) return n * 2;
-	else return score[n - 22];
-}
-
-void DFS(int inputIdx, int ans) {
-	if (inputIdx == 10) {
-		answer = max(answer, ans);
-		return;
-	}
-	for (int i = 0; i < 33; i++) {
-		if (i == 21) continue;
-		if (map[i]) {
-			int n = isNext(i, input[inputIdx]);
-			if (n == 21 || map[n] == 0) {
-				map[i]--, map[n]++;
-				DFS(inputIdx + 1, ans + getAnswer(n));
-				map[i]++, map[n]--;
-			}
-		}
-	}
-}
-
-int main() {
-	for (int i = 0; i < 10; i++) cin >> input[i];
-	map[0] = 4;
-
-	DFS(0, 0);
-	cout << answer;
-}
+//#include <iostream>
+//
+//using namespace std;
+//
+//int input[10], map[33], answer; // 0:start, 21:finsh
+//
+//int isNext(int idx, int move) {
+//	int nidx, nmove;
+//
+//	if (move == 0) return idx;
+//
+//	if (idx == 5)
+//		return isNext(22, move - 1);
+//	else if (idx == 10)
+//		return isNext(25, move - 1);
+//	else if (idx == 15)
+//		return isNext(27, move - 1);
+//	else if (idx >= 22) {
+//		if (idx < 25) {
+//			if (idx + move > 24) {
+//				nmove = move - (25 - idx);
+//				nidx = 30;
+//				return isNext(nidx, nmove);
+//			}
+//			else {
+//				return idx + move;
+//			}
+//		}
+//		else if (idx < 27) {
+//			if (idx + move > 26) {
+//				nmove = move - (27 - idx);
+//				nidx = 30;
+//				return isNext(nidx, nmove);
+//			}
+//			else {
+//				return idx + move;
+//			}
+//		}
+//		else if (idx < 30) {
+//			if (idx + move > 29) {
+//				nmove = move - (30 - idx);
+//				nidx = 30;
+//				return isNext(nidx, nmove);
+//			}
+//			else {
+//				return idx + move;
+//			}
+//		}
+//		else if (idx < 33) {
+//			if (idx + move > 32) {
+//				nmove = move - (33 - idx);
+//				nidx = 20;
+//				return isNext(nidx, nmove);
+//			}
+//			else {
+//				return idx + move;
+//			}
+//		}
+//	}
+//	else {
+//		nidx = idx + move;
+//		if (nidx > 20) return 21;
+//		return nidx;
+//	}
+//}
+//
+//int getAnswer(int n) {
+//	if (n == 21 || n == 0) return 0;
+//
+//	const int score[11] = {
+//		13,16,19,22,24,28,27,26,25,30,35
+//	};
+//
+//	if (n < 22) return n * 2;
+//	else return score[n - 22];
+//}
+//
+//void DFS(int inputIdx, int ans) {
+//	if (inputIdx == 10) {
+//		answer = max(answer, ans);
+//		return;
+//	}
+//	for (int i = 0; i < 33; i++) {
+//		if (i == 21) continue;
+//		if (map[i]) {
+//			int n = isNext(i, input[inputIdx]);
+//			if (n == 21 || map[n] == 0) {
+//				map[i]--, map[n]++;
+//				DFS(inputIdx + 1, ans + getAnswer(n));
+//				map[i]++, map[n]--;
+//			}
+//		}
+//	}
+//}
+//
+//int main() {
+//	for (int i = 0; i < 10; i++) cin >> input[i];
+//	map[0] = 4;
+//
+//	DFS(0, 0);
+//	cout << answer;
+//}
