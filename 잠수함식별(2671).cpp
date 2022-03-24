f24b32d01ecbbf1e474e1ddd94a526acece8053e
zcae1020 <zcae1020@naver.com>
2022-03-24 13:20:25 +0900

    220324v1

---
+++ 잠수함식별(2671).cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+
+int main() {
+
+    string str;
+    cin >> str;
+
+    unordered_map<string, vector<string>> umap;
+
+    umap["init"] = { "0","1" };
+    umap["0"] = { "X", "01" };
+    umap["1"] = { "10", "X" };
+    umap["01"] = { "0", "1" };
+    umap["10"] = { "100", "X" };
+    umap["100"] = { "100~", "100~1" };
+    umap["100~"] = { "100~", "100~1" };
+    umap["100~1"] = { "0", "100~1~" };
+    umap["100~1~"] = { "10 or 0", "100~1~" };
+    umap["10 or 0"] = { "100", "01" };
+
+    string state = "init";
+    for (int i = 0; i < str.length(); i++) {
+        int nextChar = str[i] - '0';
+        state = umap[state][nextChar];
+        if (state == "X") break;
+    }
+
+    if (state == "100~1" || state == "100~1~" || state == "01") cout << "SUBMARINE";
+    else cout << "NOISE";
+
+
+
+    return 0;
+}
