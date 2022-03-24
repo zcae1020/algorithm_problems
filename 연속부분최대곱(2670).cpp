5cc378d6c098023ec8281394a0084fd025b08a1a
zcae1020 <zcae1020@naver.com>
2022-03-24 10:13:55 +0900

    220324v0

---
+++ 연속부분최대곱(2670).cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int N;
+double ans, num[10000];
+
+int main() {
+	cin >> N;
+	for (int i = 0; i < N; i++) cin >> num[i];
+
+	ans = num[0];
+
+	for (int i = 1; i < N; i++) {
+		if (num[i] * num[i - 1] > num[i]) num[i] = num[i] * num[i - 1];
+
+		ans = max(ans, num[i]);
+	}
+
+	printf("%.3lf", ans);
+}
