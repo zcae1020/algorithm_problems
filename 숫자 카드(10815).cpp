5fe7f88c68997d9f787a565a1c1a350163a76290
zcae1020 <zcae1020@naver.com>
2022-04-08 13:32:42 +0900

    220408v1

--- 숫자 카드(10815).cpp
+++ 숫자 카드(10815).cpp
@@ -1,39 +1,39 @@
-#include <iostream>
-#include <algorithm>
-
-using namespace std;
-
-int N, M, arrn[500000], a;
-
-void Input() {
-	cin >> N;
-	for (int i = 0; i < N; i++) cin >> arrn[i];
-	cin >> M;
-
-	sort(arrn, arrn + N);
-}
-
-bool BS(int target) {
-	int l = 0, r = N - 1, m;
-	while (l <= r) {
-		m = (l + r) / 2;
-		if (arrn[m] == target) return true;
-		else if (arrn[m] < target) l = m + 1;
-		else r = m - 1;
-	}
-	return false;
-}
-
-void Solution() {
-	cin >> a;
-	if (BS(a)) cout << 1 << ' ';
-	else cout << 0 << ' ';
-}
-
-int main() {
-	ios::sync_with_stdio(0);
-	cin.tie(0), cout.tie(0);
-	Input();
-	while (M--)
-		Solution();
-}
+//#include <iostream>
+//#include <algorithm>
+//
+//using namespace std;
+//
+//int N, M, arrn[500000], a;
+//
+//void Input() {
+//	cin >> N;
+//	for (int i = 0; i < N; i++) cin >> arrn[i];
+//	cin >> M;
+//
+//	sort(arrn, arrn + N);
+//}
+//
+//bool BS(int target) {
+//	int l = 0, r = N - 1, m;
+//	while (l <= r) {
+//		m = (l + r) / 2;
+//		if (arrn[m] == target) return true;
+//		else if (arrn[m] < target) l = m + 1;
+//		else r = m - 1;
+//	}
+//	return false;
+//}
+//
+//void Solution() {
+//	cin >> a;
+//	if (BS(a)) cout << 1 << ' ';
+//	else cout << 0 << ' ';
+//}
+//
+//int main() {
+//	ios::sync_with_stdio(0);
+//	cin.tie(0), cout.tie(0);
+//	Input();
+//	while (M--)
+//		Solution();
+//}
