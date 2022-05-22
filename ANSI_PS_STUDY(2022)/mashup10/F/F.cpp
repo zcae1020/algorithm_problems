2ebfa243c3dd5a0b8a6d5676232e230fc9deceb4
zcae1020 <zcae1020@naver.com>
2022-05-22 19:30:25 +0900

    220522v2

--- ANSI_PS_STUDY(2022)/mashup10/F/F.cpp
+++ ANSI_PS_STUDY(2022)/mashup10/F/F.cpp
@@ -1,19 +1,82 @@
 #include <iostream>
+#include <cstring>
+#include <queue>
 
 using namespace std;
 
-void Input() {
+int dp[1001], n, k, ans, b[1000], w[1000], c[1000], knapsack[1000][15001];
 
+void fillDP()
+{
+    queue<int> q;
+    q.push(1);
+
+    while (!q.empty())
+    {
+        int cur = q.front();
+        q.pop();
+        for (int i = cur; i > 0; i--)
+        {
+            int next = cur + cur / i;
+            if (next > 1000)
+                break;
+            if (dp[next] == 0)
+            {
+                dp[next] = dp[cur] + 1;
+                q.push(next);
+            }
+        }
+    }
 }
 
-void Solution() {
+void Input()
+{
+    cin >> n >> k;
 
+    k = k >= 15000 ? 15000 : k;
+    ans = 0;
+    memset(knapsack, 0, sizeof(knapsack));
+
+    int a;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a;
+        w[i] = dp[a];
+    }
+
+    for (int i = 0; i < n; i++)
+        cin >> c[i];
 }
 
-int main() {
+void Solution()
+{
+    for (int i = w[0]; i <= k; i++)
+        knapsack[0][i] = c[0];
+
+    for (int i = 1; i < n; i++)
+    {
+        for (int j = 0; j < w[i]; j++)
+            knapsack[i][j] = knapsack[i - 1][j];
+        for (int j = w[i]; j <= k; j++)
+            knapsack[i][j] = max(knapsack[i - 1][j], knapsack[i - 1][j - w[i]] + c[i]);
+    }
+
+    cout << knapsack[n - 1][k] << '\n';
+}
+
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(NULL), cout.tie(NULL);
-    Input();
-    Solution();
-}
 
+    dp[1] = 0;
+    fillDP();
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        Input();
+        Solution();
+    }
+}
