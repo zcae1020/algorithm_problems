fa5100854907c0efa87deacd731e4d7124225564
zcae1020 <zcae1020@naver.com>
2022-05-29 14:12:12 +0900

    220529v0

--- ANSI_PS_STUDY(2022)/mashup11/D/D.cpp
+++ ANSI_PS_STUDY(2022)/mashup11/D/D.cpp
@@ -1,19 +1,61 @@
-#include <iostream>
+#include <bits/stdc++.h>
+#define FASTIO ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+#define pii pair<int, int>
+#define ll long long
+#define llu unsigned long long
 
 using namespace std;
 
-void Input() {
+void Solution()
+{
+    ll sum = 0;
+    int n;
+    queue<ll> pq;
+    map<ll, int> m;
 
+    cin >> n;
+
+    int tmp;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> tmp;
+        sum += tmp;
+        m[tmp]++;
+    }
+
+    if (n == 1)
+    {
+        cout << "YES\n";
+        return;
+    }
+
+    pq.push(sum);
+    while (pq.size() < n)
+    {
+        ll cur = pq.front();
+        pq.pop();
+
+        ll p1 = cur / 2, p2 = (cur + 1) / 2;
+        if (m[p1])
+            m[p1]--, n--;
+        else
+            pq.push(p1);
+        if (m[p2])
+            m[p2]--, n--;
+        else
+            pq.push(p2);
+    }
+    if (pq.empty())
+        cout << "YES\n";
+    else
+        cout << "NO\n";
 }
 
-void Solution() {
-
+int main()
+{
+    FASTIO
+    int tc;
+    cin >> tc;
+    while (tc--)
+        Solution();
 }
-
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL), cout.tie(NULL);
-    Input();
-    Solution();
-}
-
--- starterpack.sh
+++ starterpack.sh
@@ -1,23 +1,18 @@
 mkdir $1
 echo "#include <bits/stdc++.h>
+#define FASTIO ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
 #define pii pair<int,int>
 #define ll long long
 #define llu unsigned long long
 
 using namespace std;
 
-void Input() {
-
-}
-
 void Solution() {
 
 }
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL), cout.tie(NULL);
-    Input();
+    FASTIO
     Solution();
 }
 " > $1/$1.cpp
