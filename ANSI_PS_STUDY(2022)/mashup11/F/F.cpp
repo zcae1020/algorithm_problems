ddc7d828e4b4285274a7304f138b1a2024842447
zcae1020 <zcae1020@naver.com>
2022-05-31 17:53:20 +0900

    220531v0

--- ANSI_PS_STUDY(2022)/mashup11/F/F.cpp
+++ ANSI_PS_STUDY(2022)/mashup11/F/F.cpp
@@ -1,19 +1,84 @@
-#include <iostream>
+#include <bits/stdc++.h>
+#define FASTIO ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+#define pii pair<int, int>
+#define ll long long
+#define llu unsigned long long
 
 using namespace std;
 
-void Input() {
+void Solution()
+{
+    set<string> s1, s2;
+    int n;
 
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        string a;
+        cin >> a;
+        s1.insert(a);
+    }
+
+    for (auto a : s1)
+    {
+        reverse(a.begin(), a.end());
+        s2.insert(a);
+        if (a.size() < 3)
+            for (char c = 'a'; c <= 'z'; c++)
+                s2.insert(c + a);
+    }
+
+    for (auto a : s1)
+    {
+        if (s2.count(a))
+        {
+            cout << "YES\n";
+            return;
+        }
+    }
+
+    cout << "NO\n";
 }
 
-void Solution() {
-
+int main()
+{
+    FASTIO
+    int tc;
+    cin >> tc;
+    while (tc--)
+        Solution();
 }
 
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL), cout.tie(NULL);
-    Input();
-    Solution();
-}
 
+// 7
+// 5
+// zx
+// ab
+// cc
+// zx
+// ba
+// 2
+// ab
+// bad
+// 4
+// co
+// def
+// orc
+// es
+// 3
+// a
+// b
+// c
+// 3
+// ab
+// cd
+// cba
+// 2
+// ab
+// ab
+// 5
+// zab
+// cdd
+// cb
+// az
+// bz
--- ANSI_PS_STUDY(2022)/mashup11/G/G.cpp
+++ ANSI_PS_STUDY(2022)/mashup11/G/G.cpp
@@ -1,19 +1,44 @@
-#include <iostream>
+#include <bits/stdc++.h>
+#define FASTIO ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+#define pii pair<int, int>
+#define ll long long
+#define llu unsigned long long
 
 using namespace std;
 
-void Input() {
+const int MAX = 2e3 + 10;
+char m[MAX][MAX];
 
+void Solution()
+{
+    int n, one = 0, mx = 0;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cin >> m[i][j];
+            if (m[i][j] == '1')
+                one++;
+        }
+    }
+
+    for (int s = 0; s < n; s++)
+    {
+        int cnt = 0;
+        for (int i = 0, j = s; i < n; i++, j = (j + 1) % n)
+            if (m[i][j] == '1')
+                cnt++;
+        mx = max(mx, cnt);
+    }
+    cout << one - mx + n - mx << '\n';
 }
 
-void Solution() {
-
+int main()
+{
+    FASTIO
+    int tc;
+    cin >> tc;
+    while (tc--)
+        Solution();
 }
-
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL), cout.tie(NULL);
-    Input();
-    Solution();
-}
-
