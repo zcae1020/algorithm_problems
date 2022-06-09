8821f5c0bf41dd1f45dd86b16ee2cf12f454c6d1
zcae1020 <zcae1020@naver.com>
2022-06-09 21:24:53 +0900

    220609v0

--- ANSI_PS_STUDY(2022)/mashup12/F/F.cpp
+++ ANSI_PS_STUDY(2022)/mashup12/F/F.cpp
@@ -6,12 +6,33 @@
 
 using namespace std;
 
-void Solution() {
+const int maxn = 100;
 
+int n,arr[maxn], sarr[maxn], ans;
+
+void Solution() {
+    cin>>n;
+    for(int i=0;i<n;i++)
+        cin>>arr[i];
+
+    sarr[0] = 0;
+    for(int i=1;i<=n;i++){
+        sarr[i] = sarr[i-1]+arr[i];
+        if(!sarr[i]) break;
+        ans++;
+    }
+
+    sarr[n] = 0;
+    for(int i=n-1;i>=0;i--){
+        sarr[i] = sarr[i+1]+arr[i];
+        if(!sarr[i]) break;
+        ans++;
+    }
+
+    cout<<ans;
 }
 
 int main() {
     FASTIO
     Solution();
 }
-
--- ANSI_PS_STUDY(2022)/mashup12/G/G.cpp
+++ ANSI_PS_STUDY(2022)/mashup12/G/G.cpp
@@ -1,17 +1,64 @@
 #include <bits/stdc++.h>
 #define FASTIO ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
-#define pii pair<int,int>
+#define pii pair<int, int>
 #define ll long long
 #define llu unsigned long long
 
 using namespace std;
 
-void Solution() {
+void Solution()
+{
+    int n, m, ans = 0;
+    vector<vector<int>> v;
 
+    cin >> n >> m;
+    v.resize(n);
+
+    for (int i = 0; i < n; i++)
+    {
+        v[i].resize(m);
+        for (int j = 0; j < m; j++)
+        {
+            cin >> v[i][j];
+        }
+    }
+
+    vector<int> needSwap;
+
+    for(int i=0;i<n;i++){
+        vector<int> tv(v[i]);
+        sort(tv.begin(), tv.end());
+        for(int j=0;j<m;j++)
+            if(tv[j]!=v[i][j]) needSwap.push_back(j);
+        if(needSwap.size()) break;
+    }
+
+    if(needSwap.size()==0) ans=-1;
+    else if(needSwap.size()>2) ans=1;
+    else{
+        for(int i=0;i<n;i++){
+            swap(v[i][needSwap[0]], v[i][needSwap[1]]);
+            for(int j=0;j<m-1;j++){
+                if(v[i][j]>v[i][j+1]){
+                    ans=1;
+                    break;
+                }
+            }
+            if(ans)
+                break;
+        }
+    }
+
+    if(ans==-1) cout<<"1 1\n";
+    else if(ans==1) cout<<"-1\n";
+    else cout<<needSwap[0]+1<<' '<<needSwap[1]+1<<'\n';
 }
 
-int main() {
+int main()
+{
     FASTIO
-    Solution();
+    int tc;
+    cin >> tc;
+    while (tc--)
+        Solution();
 }
-
