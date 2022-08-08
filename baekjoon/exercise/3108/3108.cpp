9488c69df3ed84086a54cd5b2aa63ec9e6f96572
zcae1020 <zcae1020@naver.com>
2022-08-08 21:58:22 +0900

    220808v0

--- baekjoon/exercise/3108/3108.cpp
+++ baekjoon/exercise/3108/3108.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 #define FASTIO ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
-#define pii pair<int,int>
+#define pii pair<int, int>
 #define ll long long
 #define llu unsigned long long
 #define x first
@@ -8,53 +8,77 @@
 
 using namespace std;
 
-int n, par[1001], ans;
-pair<pii,pii> arr[1001];
-bool zz;
+const int maxn = 2010;
 
-bool isCross(int u, int v){
-    int ux1,ux2,uy1,uy2,vx1,vx2,vy1,vy2;
-    tie(ux1,uy1) = arr[u].first;
-    tie(ux2,uy2) = arr[u].second;
-    tie(vx1,vy1) = arr[v].first;
-    tie(vx2,vy2) = arr[v].second;
-    return (ux1<=vx1&&vx1<=ux2&&uy1<=vy1&&vy1<=uy2)!=(vx2<ux1||ux2<vx2||vy2<uy1||uy2<vy2);
+int n, arr[1000][4], ans;
+bool m[maxn][maxn], visited[maxn][maxn];
 
+int dx[4] = {0, 0, 1, -1}, dy[4] = {1, -1, 0, 0};
+
+void bfs(pii p)
+{
+    queue<pii> q;
+    q.push(p);
+    visited[p.x][p.y] = 1;
+    while (q.size())
+    {
+        pii cur = q.front();
+        q.pop();
+        for (int j = 0; j < 4; j++)
+        {
+            int nx = cur.x + dx[j], ny = cur.y + dy[j];
+            if (nx < 0 || ny < 0 || nx > 2000 || ny > 2000 || visited[nx][ny] || !m[nx][ny])
+                continue;
+
+            q.push({nx, ny});
+            visited[nx][ny] = 1;
+        }
+    }
 }
 
-int find(int u){
-    return u==par[u]?par[u]:par[u] = find(par[u]);
-}
+void Solution()
+{
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            cin >> arr[i][j];
+            arr[i][j] += 500;
+            arr[i][j] *= 2;
+        }
 
-int merge(int u, int v){
-    if(!isCross(u,v)) return 0;
+        int x1 = arr[i][0], x2 = arr[i][2], y1 = arr[i][1], y2 = arr[i][3];
 
-    u = find(u), v = find(v);
-    if(u==v) return 0;
-    par[u] = v;
-    return 1;
-}
-
-void Solution() {
-    cin>>n;
-    for(int i=1;i<=n;i++){
-        int a,b,c,d,x1,x2,y1,y2;
-        par[i]=i;
-        cin>>a>>b>>c>>d;
-        x1 = min(a,c),x2 = max(a,c),y1 = min(b,d),y2=max(b,d);
-        arr[i] = pair<pii,pii>(pii(x1,y1),pii(x2,y2));
-        if((!x1&&!y1)||(!x2&&!y2)) zz=true;
+        for (int j = x1; j <= x2; j++)
+        {
+            m[j][y1] = 1;
+            m[j][y2] = 1;
+        }
+        for (int j = y1; j <= y2; j++)
+        {
+            m[x1][j] = 1;
+            m[x2][j] = 1;
+        }
     }
 
-    ans=zz?n-1:n;
-    for(int i=1;i<=n;i++)
-        for(int j=i+1;j<=n;j++)
-            ans-=merge(i,j);
-    
-    cout<<ans;
+    if (m[1000][1000])
+        ans--;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (!visited[arr[i][0]][arr[i][1]])
+        {
+            ans++;
+            bfs({arr[i][0], arr[i][1]});
+        }
+    }
+
+    cout << ans;
 }
 
-int main() {
+int main()
+{
     FASTIO
     Solution();
 }
