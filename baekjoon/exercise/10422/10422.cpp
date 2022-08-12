7290c8da21d50e65434454f0d965b360b0a86276
zcae1020 <zcae1020@naver.com>
2022-08-12 19:33:31 +0900

    220812v0

--- baekjoon/exercise/10422/10422.cpp
+++ baekjoon/exercise/10422/10422.cpp
@@ -10,38 +10,22 @@ using namespace std;
 
 const ll maxl = 5010, MOD = 1e9 + 7;
 
-/*
-i=2: 1
-i=4: 1 1
-i=6: 1 2 1
-i=8: 1 3 3 1
-i=10: 1 4 6 4 1
-    .
-    .
-    .
-
-*/
-
 int t;
-ll dp[maxl][maxl], rdp[maxl];
+ll dp[maxl];
 
 void Solution()
 {
-    for (int i = 2; i <= 5000; i += 2)
-    {
-        dp[i][0] = dp[i][(i - 1) / 2] = 1;
-        for (int j = 1; j < (i - 1) / 2; j++)
-            dp[i][j] = (dp[i - 2][j - 1] + dp[i - 2][j]) % MOD;
-        for (int j = 0; j <= (i - 1) / 2; j++)
-            rdp[i] = (rdp[i] + dp[i][j]) % MOD;
-    }
+    dp[0] = dp[2] = 1;
+    for (int i = 4; i <= 5000; i += 2)
+        for (int j = 0; j <= i - 2; j += 2)
+            dp[i] = (dp[i] + dp[i - 2 - j] * dp[j]) % MOD;
 
     cin >> t;
     while (t--)
     {
         int x;
         cin >> x;
-        cout << rdp[x] << '\n';
+        cout << dp[x] << '\n';
     }
 }
 
