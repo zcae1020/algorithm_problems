54c9cb5302fc80d713e0dbb7eae6ff57c57631a4
zcae1020 <zcae1020@naver.com>
2022-09-01 00:22:30 +0900

    220901v0

--- baekjoon/exercise/13975/13975.cpp
+++ baekjoon/exercise/13975/13975.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 #define FASTIO ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
-#define pii pair<int,int>
+#define pii pair<int, int>
 #define ll long long
 #define llu unsigned long long
 #define fs first
@@ -8,39 +8,41 @@
 
 using namespace std;
 
-const int maxK = 1e6+10;
+int T, K;
 
-int T,K, psum[maxK];
-unordered_map<ll, ll> dp;
+void Solution()
+{
+    cin >> T;
+    while (T--)
+    {
+        priority_queue<ll> pq;
+        ll x1, x2, ans = 0;
 
-void Solution() {
-    cin>>T;
-    while(T--){
-        cin>>K;
-        for(int i=0;i<K;i++)
-            cin>>dp[(i<<20)+i];
+        cin >> K;
 
-        psum[0] = dp[0];
-        for(int i=1;i<K;i++)
-            psum[i] = psum[i-1]+dp[(i<<20)+i];
-        
-        for(int l = 1;l<=K;l++){
-            for(int s = 0;s+l<K;s++){
-                dp[(s<<20)+s+l] = 1e18;
-                for(int m = s;m<s+l;m++){
-                    dp[(s<<20)+s+l] = min(dp[(s<<20)+s+l], 
-                        dp[(s<<20)+m]+dp[((m+1)<<20)+s+l]+(s==m?0:dp[(s<<20)+m])+(m+1==s+l?0:dp[((m+1)<<20)+s+l]));
-                }
-            }
+        for (int i = 0; i < K; i++)
+        {
+            cin >> x1;
+            pq.push(-x1);
         }
 
-        cout<<dp[K-1]<<'\n';
+        while (pq.size() > 1)
+        {
+            x1 = -pq.top();
+            pq.pop();
+            x2 = -pq.top();
+            pq.pop();
 
-        dp.clear();
+            ans += x1 + x2;
+            pq.push(-(x1 + x2));
+        }
+
+        cout << ans << '\n';
     }
 }
 
-int main() {
+int main()
+{
     FASTIO
     Solution();
 }
