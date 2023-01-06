c875cdb33c615d028a3f2b9694f3fa95e5c4f90f
zcae1020 <zcae1020@naver.com>
2023-01-06 23:27:15 +0900

    230106v0

---
+++ baekjoon/exercise/10926/10926.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#define FASTIO ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+#define pii pair<int, int>
+#define ll long long
+#define llu unsigned long long
+#define fs first
+#define sd second
+
+using namespace std;
+
+string s;
+
+void Solution()
+{
+    cin >> s;
+    cout << s + "??!";
+}
+
+int main()
+{
+    FASTIO
+    Solution();
+}
