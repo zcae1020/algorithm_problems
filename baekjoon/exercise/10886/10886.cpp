a15814b344a62dbc492a256b3f57dcf310b3bc85
zcae1020 <zcae1020@naver.com>
2023-04-16 22:23:02 +0900

    230416v1

--- baekjoon/exercise/10886/10886.cpp
+++ baekjoon/exercise/10886/10886.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 #define FASTIO ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
-#define pii pair<int,int>
+#define pii pair<int, int>
 #define ll long long
 #define llu unsigned long long
 #define fs first
@@ -8,8 +8,16 @@
 
 using namespace std;
 
-void Solution() {
+int n, a, isCute[2];
 
+void Solution() {
+    cin >> n;
+    while (n--) {
+        cin >> a;
+        isCute[a]++;
+    }
+
+    cout << (isCute[0] > isCute[1] ? "Junhee is not cute!" : "Junhee is cute!");
 }
 
 int main() {
